c5048331fbbb9940e92c871fcc7c179467ec31b5
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-08-26 20:23:47 -0600

    Add Book constructor in js

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,19 @@
 console.log("Hi, Mom!");
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator")
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`
+    }
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+
+console.log(theHobbit.info());
